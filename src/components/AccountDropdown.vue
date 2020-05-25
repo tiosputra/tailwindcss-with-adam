fb7ee0dca1ff807136cc565abf9d7209477bcbdb
karyaTio <karya.tiosaputra@gmail.com>
2020-05-25 10:53:42 +0700

    making the dropdown interactive

--- src/components/AccountDropdown.vue
+++ src/components/AccountDropdown.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="relative">
     <button
-      class="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white"
+      @click="isOpen = !isOpen"
+      class="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white"
     >
       <img
         class="h-full w-full object-cover"
@@ -9,7 +10,15 @@
         alt="Your avatar"
       />
     </button>
-    <div class="absolute right-0 mr-2 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+
+    <button
+      v-if="isOpen"
+      @click="isOpen = false"
+      tabindex="-1"
+      class="fixed inset-0 bg-black opacity-25 w-full h-full cursor-default"
+    ></button>
+
+    <div v-if="isOpen" class="absolute right-0 mr-2 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
       <a
         href="#"
         class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
@@ -24,5 +33,24 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  created() {
+    const handleEscape = e => {
+      if (e.key === "Esc" || e.key === "Escape") {
+        this.isOpen = false;
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+
+    this.$once("hook:beforeDestroy", () => {
+      document.removeEventListener("keydown", handleEscape);
+    });
+  }
+};
 </script>
